e1838987ff53e93a1a9c95dab47c4bfbb9b5fe1d
seungchanlee1128 <seungchanlee123@gmail.com>
2025-11-06 16:45:28 +0900

    Thymeleaf model 방법 사용

--- src/main/kotlin/kr/ac/kumoh/s20220832/w25w08_thymeleaf/controller/SongController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20220832/w25w08_thymeleaf/controller/SongController.kt
@@ -1,11 +1,13 @@
 package kr.ac.kumoh.s20220832.w25w08_thymeleaf.controller
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class SongController {
     @GetMapping("/songs/random")
-    fun getRandomSong(): String {
+    fun getRandomSong(model: Model): String {
+        model.addAttribute("songtitle", "sodapop")
         return "random"
     }
 }
